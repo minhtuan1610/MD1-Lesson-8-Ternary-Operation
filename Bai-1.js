ae99adb9fe2a560c5dc519dd4ce7d4765660bce8
Nguyễn Minh Tuấn <tuannm1610@gmail.com>
2022-01-09 22:15:01 +0700

    Toan tu ba ngoi

---
+++ Bai-1.js
@@ -0,0 +1,4 @@
+let a = Number(prompt("a: "));
+let b = Number(prompt("b: "));
+let result = a + b;
+(result < 4) ? alert("Below") : alert("Over");
---
+++ Bai-2.js
@@ -0,0 +1,16 @@
+// let inputMessage = prompt("Type here");
+// let message;
+// if (inputMessage == "Employee") {
+//     alert(message = "Hello");
+// } else if (inputMessage == "Director") {
+//     alert(message = "Greetings");
+// } else if (inputMessage == "") {
+//     alert(message = "No login");
+// } else {
+//     alert(message = "");
+// }
+//Su dung toan tu ba ngoi
+let inputMessage = prompt("Type here");
+(inputMessage == "Employee") ? alert("Hello") :
+    (inputMessage == "Director") ? alert("Greetings") :
+        (inputMessage == "") ? alert("No login") : alert("");
